3778fbf402a999350d144767a407a6e659529d5b
keeganthomp <whereiskeegan@gmail.com>
2019-04-12 19:03:54 -0500

    changing sequelize things

--- server/database/Tart.js
+++ server/database/Tart.js
@@ -1,14 +1,8 @@
 const Sequelize = require('sequelize')
+var database = process.env.DATABASE_URL || 'tart'
 
-let sequelize = new Sequelize('tart', 'keegan', 'hu8jmn3', {
-  host: '0.0.0.0',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+const sequelize = new Sequelize(database, 'postgres', '', {
+  dialect: 'postgres'
 })
 
 module.exports = {
