29e9c569d5217d234ab05a4eb5924c3b0da3be03
keeganthomp <whereiskeegan@gmail.com>
2019-04-14 12:23:31 -0500

    removing debugging logs

--- server/database/Tart.js
+++ server/database/Tart.js
@@ -6,8 +6,6 @@ const productionDbConfig = process.env.DATABASE_URL
 const dbUser = process.env.POSTGRES_USER
 const dbPassword = process.env.POSTGRES_PASSWORD
 
-console.log('IS PRODUCITON:', isProdution)
-
 const sequelizeConfig = isProdution
   ? productionDbConfig
   : {
